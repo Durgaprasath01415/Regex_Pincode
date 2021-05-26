d595c8090f2c7f771b73f6fbfe5b74904a7c4875
Durgaprasath01415 <83449179+Durgaprasath01415@users.noreply.github.com>
2021-05-26 08:01:32 +0530

    ADDED:Second pincode and its pattern

--- src/main/java/PinCode.java
+++ src/main/java/PinCode.java
@@ -3,12 +3,22 @@ import java.util.regex.Pattern;
 
 public class PinCode {
     public static void main(String[] args) {
+
         String pinCode = "400088";
         String patternNum = "^[0-9]{6}$";
+        System.out.println( pinCode+ " : " +checkpattern(pinCode,patternNum));
+        String pinCode1 = "A400088";
+        String patternNum1 = "(^[0-9]{6})";
+        System.out.println( pinCode1+ " : " +checkpattern(pinCode1,patternNum1));
+
+
+    }
+    public static boolean checkpattern(String pinCode,String patternNum){
         Pattern pattern = Pattern.compile(patternNum);
         Matcher matcher = pattern.matcher(pinCode);
-        if (matcher.find()){
-            System.out.println("valid");
+        if (matcher.find()) {
+            return true;
         }
+        return false;
     }
 }
