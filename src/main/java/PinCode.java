e5e3c083641e262774e3815bef1ef3c79d1f772d
Durgaprasath01415 <83449179+Durgaprasath01415@users.noreply.github.com>
2021-05-26 07:30:41 +0530

    REFACTORED: Using method checkpattern

--- src/main/java/PinCode.java
+++ src/main/java/PinCode.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -5,10 +6,18 @@ public class PinCode {
     public static void main(String[] args) {
         String pinCode = "400088";
         String patternNum = "^[0-9]{6}$";
+        if( checkpattern(pinCode,patternNum)) {
+            System.out.println("valid");
+            return;
+        }
+        System.out.println("invalid");
+    }
+        public static boolean checkpattern(String pinCode,String patternNum){
         Pattern pattern = Pattern.compile(patternNum);
         Matcher matcher = pattern.matcher(pinCode);
-        if (matcher.find()){
-            System.out.println("valid");
+        if (matcher.find()) {
+            return true;
         }
+    return false;
     }
 }
